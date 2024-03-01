52a4b7d3c8480df4061f37fa1a52a5faff8520bb
Roger <morenoroger001@gmail.com>
2024-03-01 17:36:35 -0500

    feat: Add grammar class

---
+++ code/parser/grammar/temp.py
@@ -0,0 +1,15 @@
+from grammar import Grammar
+
+# Grammar example
+G = Grammar()
+E = G.NonTerminal('E', True)
+T,F,X,Y = G.NonTerminals('T F X Y')
+plus, minus, star, div, opar, cpar, num = G.Terminals('+ - * / ( ) num')
+
+E %= T + X
+X %= plus + T + X | minus + T + X | G.Epsilon
+T %= F + Y
+Y %= star + F + Y | div + F + Y | G.Epsilon
+F %= num | opar + E + cpar
+
+print(G)
