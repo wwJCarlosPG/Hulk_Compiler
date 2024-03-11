9a2b9bcb2a0478bc8202ab1363a882df0c71030b
Roger <morenoroger001@gmail.com>
2024-03-10 22:34:31 -0400

    feat: Add LR(0) automaton build function
    - Add Item and State classes

--- code/parser/grammar/temp.py
+++ code/parser/grammar/temp.py
@@ -1,19 +1,30 @@
-from utils import compute_firsts, compute_follows
+from Item import Item
+from utils import compute_firsts, compute_follows, build_LR0_automaton
 from grammar import Grammar
 
-# Grammar example
+# Grammar a^{n}b^{n}
 G = Grammar()
-E = G.NonTerminal('E', True)
-T,F,X,Y = G.NonTerminals('T F X Y')
-plus, minus, star, div, opar, cpar, num = G.Terminals('+ - * / ( ) num')
+S = G.NonTerminal('S', True)
+a, b = G.Terminals('a b')
 
-E %= T + X
-X %= plus + T + X | minus + T + X | G.Epsilon
-T %= F + Y
-Y %= star + F + Y | div + F + Y | G.Epsilon
-F %= num | opar + E + cpar
+S %= a + S + b | a + b
 
 print(G)
 
 firsts = compute_firsts(G)
 compute_follows(G, firsts)
+
+# Extend Grammar with S'-> E
+GG = G.AugmentedGrammar()
+
+# Build NFA LR(0) automaton
+NFA = build_LR0_automaton(GG)
+
+# Write automaton
+dot_graph = NFA.graph().write("nfa.dot", format='raw', encoding='utf-8')
+
+# Convert to FDA LR(0) automaton
+DFA = NFA.to_deterministic()
+
+# Write automaton
+dot_graph = DFA.graph().write("dfa.dot", format='raw', encoding='utf-8')
--- code/parser/grammar/utils.py
+++ code/parser/grammar/utils.py
@@ -1,7 +1,19 @@
 from ContainerSet import ContainerSet
+from Item import Item
+from automata import State
+from grammar import Grammar
 from definitions import NonTerminal, Sentence
 
-def compute_firsts(G):
+def compute_firsts(G: Grammar):
+    """
+    Compute the First sets for the given grammar.
+
+    Parameters:
+    - G: The grammar for which First sets are to be computed.
+
+    Returns:
+    - dict: A dictionary containing the computed First sets for terminals, non-terminals, and sentences.
+    """
     firsts = {}
     change = True
     
@@ -81,7 +93,17 @@ def _compute_local_first(firsts, alpha):
     return first_alpha
 
 
-def compute_follows(G, firsts):
+def compute_follows(G: Grammar, firsts):
+    """
+    Compute the Follow sets for the given grammar using the computed First sets.
+
+    Parameters:
+    - G: The grammar for which Follow sets are to be computed.
+    - firsts: The precomputed First sets for terminals, non-terminals, and sentences.
+
+    Returns:
+    - dict: A dictionary containing the computed Follow sets for non-terminals.
+    """
     follows = { }
     change = True
     
@@ -127,3 +149,70 @@ def compute_follows(G, firsts):
             file.write(f"{key}: {value}\n")
 
     return follows
+
+
+
+def build_LR0_automaton(G: Grammar):
+    """
+    Build the LR(0) automaton for the given augmented grammar.
+
+    Parameters:
+    - G (Grammar): An augmented grammar.
+
+    Returns:
+    - State: The initial state of the LR(0) automaton.
+    
+    Raises:
+    - AssertionError: If the grammar does not meet the required conditions (single start symbol production and augmented grammar).
+    """
+    assert len(G.startSymbol.productions) == 1 and G.IsAugmentedGrammar, 'Grammar must be augmented'
+
+    start_production = G.startSymbol.productions[0]
+    start_item = Item(start_production, 0)
+
+    automaton = State(start_item, final=True)
+
+    pending = [ start_item ]
+    # Contains for each Item, the corresponding State
+    visited = { start_item: automaton }
+
+    while pending:
+        current_item = pending.pop()
+        if current_item.IsReduceItem:
+            continue
+        
+        next_symbol = current_item.NextSymbol
+        next_item = current_item.NextItem()
+
+        # Transitions with next symbol
+        if next_symbol:
+            current_state = visited[current_item]
+
+            # ~~~~~~~~~~~~~~ Explicit transition ~~~~~~~~~~~~~~
+            if not next_item in visited:
+                pending.append(next_item)
+                visited[next_item] = State(next_item, final=True)
+
+            # (E -> .T+E) -T-> (E -> T.+E)
+            # (E -> T.+E) -+-> (E -> T+.E)
+            dest_state = visited[next_item]
+            current_state.add_transition(next_symbol, dest_state)
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+            # ~~~~~~~~~~~~~~~ Epsilon transition ~~~~~~~~~~~~~~~
+            if next_symbol.IsNonTerminal:
+                epsilon_destination_productions = [p for p in G.Productions if p.Left == next_symbol]
+                epsilon_destination_items = [Item(p, 0) for p in epsilon_destination_productions]
+
+                for dest_item in epsilon_destination_items:
+                    if not dest_item in visited:
+                        pending.append(dest_item)
+                        visited[dest_item] = State(dest_item, final=True)
+                    
+                    # (E -> .T+E) -e-> (T -> .alpha)
+                    dest_state = visited[dest_item]
+                    current_state.add_epsilon_transition(dest_state)
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                
+    return automaton
