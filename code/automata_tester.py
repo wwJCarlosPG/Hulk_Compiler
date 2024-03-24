c0e71c1ecbea99337be70b1f27d84b04ae955fa2
jancarlospg <wwjancarlospg2000@gmail.com>
2024-03-24 15:55:43 -0400

    feat: add some tokens

--- code/automata_tester.py
+++ code/automata_tester.py
@@ -1,15 +1,41 @@
 from lexer.lexer import Lexer
-
+s = [ 
+    '_', 
+    ',',
+    ':',
+    ';',
+    '<',
+    '>',
+    '?',
+    '!',
+    '%',
+    '.',
+    '\n',
+    '\t'
+    ]
+symbols = '|'.join(str(n) for n in s)
+print(symbols)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 nonzero_digits = '|'.join(str(n) for n in range(1,10))
 zero_digits = '|'.join(str(n) for n in range(0,10))
+print(zero_digits)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 letters = '|'.join(chr(n) for n in range(ord('a'),ord('z')+1))
-letters += letters.join('|')
-letters += '|'.join(chr(n) for n in range(ord('A'),ord('Z')+1))
-alphanumeric = f'({letters}|{nonzero_digits})'
 print(letters)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+letters += letters.join('|')
+print(letters)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+letters += '|'.join(chr(n) for n in range(ord('A'),ord('Z')+1))
+print(letters)
+print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+alphanumeric = f'{letters}|{zero_digits}'
+alphanumeric_with_spaces = alphanumeric
+alphanumeric_with_spaces+='|'+ ' '
+alphanumeric_with_spaces+='|'+symbols
+print(alphanumeric_with_spaces)
 lexer = Lexer([
-    ('str', f'((")({alphanumeric})*("))'),
-    ('num', f'(({nonzero_digits})(0|{nonzero_digits})*)'),
+    ('str', f'((")({alphanumeric_with_spaces})*("))'),
     ('plus', '+'),
     ('minus', '-'),
     ('star', '\*'),
@@ -44,21 +70,21 @@ lexer = Lexer([
     ('for','for'),
     ('new','new'),
     ('type','type'),      
-    ('id', f'({letters})*'),
+    ('id', f'(({letters})({alphanumeric})*)'),
+    ('num', f'(({nonzero_digits})({zero_digits})*)'),
     ('concat','@'),
     ('concatX2','@@')
 ], 'eof')
 text = 'let    x=10,y=202 in (332823948*xiom304230)'
-text = 'while(x<=new 104){ \n if (x!=136) x = @4 else x="asss5" }'
-text = 'let    xz=10,y=222 in (332823948*xiom304230)'
+text = 'while(x<=new 104) "x","y" { if (x!=136) x = @4 else x="mas_ss5, mk, type j." }'
+#text = 'let    xz=10,y="hello hs" in (332823948*xiom304203)'
 print(f'\n>>> Tokenizando: "{text}"')
 tokens = lexer(text)
 print(tokens)
 
 
 # issues:
-# don't recognize a one digit number
-# don't recognize the strings with letters and digits
+# don't recognize \" within str
 #
 #
 #
