edf256038096bdeef6b2842ef9dc966ab5c9633d
Roger <morenoroger001@gmail.com>
2024-03-24 14:12:05 -0400

    fix: Remove try from SLR1Parser

--- code/parser_tester.py
+++ code/parser_tester.py
@@ -17,12 +17,12 @@ match selector:
         tokenized_string = [G['int'], G['*'], G['('], G['int'], G['+'], G['int'], G[')'], G['$']]
     case 3:
         G = get_hulk()
+        tokenized_string = [G['num'], G['*'], G['('], G['num'], G['+'], G['num'], G[')'], G['$']]
     case _:
         raise Exception("No match case")
 
 # lr1_parser = LR1Parser(G)
 slr1_parser = SLR1Parser(G, verbose=True)
-# output, operations = slr1_parser(tokenized_string)
-
-# print(f'\nDerivations sequence: {output}')
-# print(f'\nOperations sequence: {operations}')
+output, operations = slr1_parser(tokenized_string)
+print(f'\nDerivations sequence: {output}')
+print(f'\nOperations sequence: {operations}')
